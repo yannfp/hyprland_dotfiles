ddfc51aee5820357d4543d0cdb71c77e26584439
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2025-11-15 18:36:03 +0100

    feat: hyprland & rofi & waybar config

---
+++ hypr/powermenu.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+dir="~/.config/hypr/rofi"
+uptime=$(uptime -p | sed -e 's/up //g')
+
+rofi_command="rofi -no-config -theme $dir/powermenu.rasi"
+
+# Options
+shutdown=" Shutdown"
+reboot=" Restart"
+lock=" Lock"
+suspend=" Sleep"
+logout=" Logout"
+
+# Variable passed to rofi
+options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
+case $chosen in
+    
+	$shutdown)
+		systemctl poweroff
+        ;;
+
+    $reboot)
+		systemctl reboot
+		;;
+
+	$lock)
+		hyprlock
+		;;
+
+	$suspend)
+		mpc -q pause
+		amixer set Master mute
+		systemctl suspend
+		;;
+
+    $logout)
+		loginctl terminate-session $XDG_SESSION_ID
+        ;;
+esac
